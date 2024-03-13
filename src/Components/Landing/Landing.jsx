f957f6e56ec61f44a71ecfbdd47c3523773ab8c0
Kris Kurzawa <kurzawa1@yahoo.com>
2024-03-12 21:53:22 -0400

    mobile landing styling

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -3,13 +3,22 @@ import Logo from '/favicon.png'
 
 const Landing = () => {
     return (
-        <main className='h-screen flex justify-center'>
-            <article className='fade-out flex justify-center items-center h-screen'>
-                <img src={Logo} alt="logo" className='logo' />
-                <h2 className='logo-text text-2xl lg:text-4xl'>Web Devloper</h2>
-            </article>
-
-        </main>
+        <>
+            {/* md and lg  */}
+            <main className='hidden h-screen md:flex justify-center'>
+                <article className='fade-out flex justify-center items-center h-screen'>
+                    <img src={Logo} alt="logo" className='logo' />
+                    <h2 className='logo-text text-2xl lg:text-4xl'>Web Devloper</h2>
+                </article>
+            </main>
+            {/* sm  */}
+            <main className='h-screen flex justify-center md:hidden'>
+                <article className='sm-fade-out flex justify-center items-center h-screen'>
+                    <img src={Logo} alt="logo" className='sm-logo' />
+                    <h2 className='sm-logo-text text-2xl lg:text-4xl'>Web Devloper</h2>
+                </article>
+            </main>
+        </>
     )
 }
 
