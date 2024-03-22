507c83629d9c9b2d3c49fca4798ca721db74b6b6
Kris Kurzawa <kurzawa1@yahoo.com>
2024-03-22 14:30:00 -0400

    cleaned landing css

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -5,17 +5,17 @@ const Landing = () => {
     return (
         <>
             {/* md and lg  */}
-            <main className='landing-main-container hidden h-screen md:flex justify-center'>
-                <article className='fade-out flex justify-center items-center h-screen'>
+            <main className='landing-main-container hidden md:flex justify-center w-full h-screen'>
+                <article className='fade-out flex flex-col justify-center items-center h-screen'>
                     <img src={Logo} alt="logo" className='logo' />
-                    <h2 className='logo-text md:text-5xl text-[#eed29b]'>Web Devloper</h2>
+                    <h2 className='logo-text md:text-5xl'>Web Devloper</h2>
                 </article>
             </main>
             {/* sm  */}
-            <main className='sm-landing-main-container h-screen flex justify-center md:hidden'>
-                <article className='sm-fade-out flex justify-center items-center h-screen '>
+            <main className='sm-landing-main-container flex justify-center md:hidden w-full h-screen'>
+                <article className='sm-fade-out flex flex-col justify-center items-center w-full h-screen '>
                     <img src={Logo} alt="logo" className='sm-logo' />
-                    <h2 className='sm-logo-text text-3xl lg:text-5xl text-[#eed29b]'>Web Devloper</h2>
+                    <h2 className='sm-logo-text text-3xl lg:text-5xl'>Web Devloper</h2>
                 </article>
             </main>
         </>
