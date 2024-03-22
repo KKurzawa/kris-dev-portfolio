d700274225381c9e0218c0c4501942c978c221f2
Kris Kurzawa <kurzawa1@yahoo.com>
2024-03-21 22:17:51 -0400

    updated landing page

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -5,14 +5,14 @@ const Landing = () => {
     return (
         <>
             {/* md and lg  */}
-            <main className='landing-main-container hidden h-screen md:flex justify-center bg-[#980D1C]'>
+            <main className='landing-main-container hidden h-screen md:flex justify-center'>
                 <article className='fade-out flex justify-center items-center h-screen'>
                     <img src={Logo} alt="logo" className='logo' />
                     <h2 className='logo-text md:text-5xl text-[#eed29b]'>Web Devloper</h2>
                 </article>
             </main>
             {/* sm  */}
-            <main className='sm-landing-main-container h-screen bg-[#980D1C] flex justify-center md:hidden'>
+            <main className='sm-landing-main-container h-screen flex justify-center md:hidden'>
                 <article className='sm-fade-out flex justify-center items-center h-screen '>
                     <img src={Logo} alt="logo" className='sm-logo' />
                     <h2 className='sm-logo-text text-3xl lg:text-5xl text-[#eed29b]'>Web Devloper</h2>
