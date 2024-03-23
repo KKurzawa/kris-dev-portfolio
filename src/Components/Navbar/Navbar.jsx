7e96439df919ded4a4d0110221781843d86d4919
Kris Kurzawa <kurzawa1@yahoo.com>
2024-03-23 14:36:47 -0400

    update to landing

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,15 +4,15 @@ import { Navlinks } from '../../assets/data/NavLinks';
 const Navbar = () => {
 
     return (
-        <main id='navbar' className='bg-[#100607]'>
-            <nav className='hidden h-32 md:flex'>
-                <header className='flex flex-col justify-center w-1/3 pl-16 h-full'>
-                    <h1 className='kurzawa text-[#980D1C] text-2xl md:text-6xl'>KURZAWA</h1>
-                    <h2 className='text-[#eed29b] text-3xl'>DEVELOPMENT</h2>
+        <main id='navbar' className=''>
+            <nav className='hidden md:flex h-32'>
+                <header className='flex flex-col justify-center w-1/3 h-full pl-16'>
+                    <h1 className='kurzawa text-2xl md:text-6xl'>KURZAWA</h1>
+                    <h2 className='development text-3xl'>DEVELOPMENT</h2>
                 </header>
-                <ol className='nav-link-container flex justify-evenly h-full items-end w-2/3'>
+                <ol className='nav-link-container flex justify-evenly items-end w-2/3 h-full'>
                     {Navlinks.map((item) => (
-                        <li key={item.id} onClick='' className='nav-link text-[#eed29b] text-3xl mb-4'>
+                        <li key={item.id} className='nav-link mb-4 text-3xl'>
                             <a href={item.link} className=''>
                                 {item.title}
                             </a>
--- src/Components/NavbarMobile/NavbarMobile.jsx
+++ src/Components/NavbarMobile/NavbarMobile.jsx
@@ -12,24 +12,23 @@ const NavbarMobile = () => {
     useClickAway(ref, () => setIsOpen(false))
 
     return (
-        <main id='navbar-mobile' className='bg-[#100607]'>
-            {/* small */}
-            <nav ref={ref} className='h-20 flex justify-between items-center w-full md:hidden'>
-                <header className='flex flex-col justify-center w-1/3 pl-8 h-full'>
-                    <h1 className='kurzawa text-[#980D1C] text-2xl'>KURZAWA</h1>
-                    <h2 className='text-[#eed29b] text-sm'>DEVELOPMENT</h2>
+        <main id='navbar-mobile' className='navbar-mobile'>
+            <nav ref={ref} className='md:hidden flex justify-between items-center w-full h-20'>
+                <header className='flex flex-col justify-center w-1/3 h-full pl-8'>
+                    <h1 className='kurzawa text-2xl'>KURZAWA</h1>
+                    <h2 className='development text-sm'>DEVELOPMENT</h2>
                 </header>
                 <article className='pr-2'>
                     <Hamburger toggled={isOpen} size={30} toggle={setIsOpen} color='#eed29b' rounded easing='ease-in' />
                 </article>
                 <AnimatePresence>
                     {isOpen && (
-                        <motion.div
+                        <motion.nav
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             exit={{ opacity: 0 }}
                             transition={{ duration: 1 }}
-                            className='absolute left-0 right-0 bg-[#100607] top-20 p-5'
+                            className='dropdown absolute left-0 right-0 top-20 p-5'
                         >
                             <ul className='grid gap-2'>
                                 {Navlinks.map((route, idx) => {
@@ -49,17 +48,17 @@ const NavbarMobile = () => {
                                             <a
                                                 onClick={() => setIsOpen((prev) => !prev)}
                                                 className={
-                                                    "flex items-center justify-between w-full p-[.6rem] rounded-lg bg-[#100607]"
+                                                    "navlink flex items-center justify-between w-full p-[.6rem] rounded-lg"
                                                 }
                                                 href={route.link}
                                             >
-                                                <span className="flex gap-1 text-lg text-[#eed29b]">{route.title}</span>
+                                                <span className="flex gap-1 text-lg">{route.title}</span>
                                             </a>
                                         </motion.li>
                                     );
                                 })}
                             </ul>
-                        </motion.div>
+                        </motion.nav>
                     )}
                 </AnimatePresence>
             </nav>
